19bc1393675f27c13ab1f17b300d8c58c6a5fa10
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-06 17:21:26 +0500

    подключена форма из примера

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,3 +1,12 @@
-<div>
-    fooooooooooorm
+<div class='w-1/2 m-auto'>
+    <form wire:submit="create">
+        {{ $this->form }}
+        
+        <button type="submit" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Submit
+        </button>
+        <x-filament-actions::modals />
+    </form>
+    
+    <x-filament-actions::modals />
 </div>
